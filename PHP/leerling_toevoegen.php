b80b3b0caf5456e1f11a80ac49ef85300439cc5e
JoeriDekker <73581036+JoeriDekker@users.noreply.github.com>
2020-11-17 15:51:21 +0100

    CSS voor toevoegen

--- PHP/leerling_toevoegen.php
+++ PHP/leerling_toevoegen.php
@@ -1,21 +1,81 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>leerling toevoeg!</title>
+    <title>Leerling Toevoegen!</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="../CSS/toevoeg.css">
+    
+    
 </head>
-
 <body>
-<form action="leerling_toevoegen_verwerk.php" method="post">
-    <label for="voornaam">Voornaam</label>
-    <input name="voornaam" id="voornaam" required>
-    <label for="achternaam">achternaam</label>
-    <input name="achternaam" id="achternaam" required>
-    <label for="klas">klas</label>
-    <input name="klas" id="klas" required>
-    <input type="submit" name="submit" value="submit">
-</form>
 
+<nav class="navbar navbar-expand-md shadow-lg bg-white">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-dark" href="#">Hallo, Meneer Remeeus</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto text-dark" href="dashboard.php">Dashboard</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="loguit.php">Uitloggen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+
+<div class="toevoegen">
+
+    <form action="leerling_toevoegen_verwerk.php" method="post" class="toevoeg-form">
+        <h1>Leerling Toevoegen</h1>
+        <div class="textb">
+            <input name="voornaam" id="voornaam" required>
+            <div class="placeholder">Voornaam</div>
+        </div>
+
+        <div class="textb">
+            <input name="achternaam" id="achternaam" required>
+            <div class="placeholder">Achternaam</div>
+        </div>
+
+        <div class="textb">
+            <input name="klas" id="klas" required>  
+            <div class="placeholder">Klas</div>
+        </div>
+
+
+        <br>
+
+        <button class="btn fas fa-arrow-right" type="submit" name="submit" value="Verzenden" >Versturen</button>
+
+  </form>
+</div>  
+
+
+    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
